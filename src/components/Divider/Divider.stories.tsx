25676409c99d02b3f4024648f7a44d007db78df9
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-11-26 21:07:11 -0500

    Fixed divider

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -15,5 +15,13 @@ const Template: ComponentStory<typeof Divider> = (args) => (
 );
 
 export const Default = Template.bind({});
+export const WithLabel = Template.bind({});
+export const LabelLeft = Template.bind({});
+export const LabelRight = Template.bind({});
+export const ThickDivider = Template.bind({});
 
 Default.args = {};
+WithLabel.args = { label: "It has a label" };
+LabelLeft.args = { label: "It has a label", labelPosition: "left" };
+LabelRight.args = { label: "It has a label", labelPosition: "right" };
+ThickDivider.args = { label: "Thick red divider", width: 20, color: "red" };
