c8a222806af6e193dfca41ac99e56bbf653a839b
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-11-28 18:10:50 -0500

    Added examples to SB

--- src/components/Buttons/Button/button.stories.tsx
+++ src/components/Buttons/Button/button.stories.tsx
@@ -3,20 +3,57 @@ import { ComponentStory } from "@storybook/react";
 import "@storybook/addon-console";
 
 import Button from "./Button";
-import useTheme from "../../hooks/useTheme";
+import Flex from "../../Layout/Flex";
 
 export default {
   title: "Components/Buttons/Button",
   component: Button,
 };
 
-const Template: ComponentStory<typeof Button> = (args) => {
-  return <Button {...args} />;
+export const Examples: ComponentStory<typeof Button> = () => {
+  return (
+    <div>
+      <Flex>
+        <Button>Default</Button>
+        <Button variant="outlined">Outlined</Button>
+        <Button variant="subtle">Subtle</Button>
+        <Button loading={true}>Loading</Button>
+      </Flex>
+      <Flex>
+        <Button color="red">Default</Button>
+        <Button color="red" variant="outlined">
+          Outlined
+        </Button>
+        <Button color="red" variant="subtle">
+          Subtle
+        </Button>
+        <Button color="red" loading={true}>
+          Loading
+        </Button>
+      </Flex>
+      <Flex>
+        <Button borderRadius="sm" color="red">
+          sm border
+        </Button>
+        <Button borderRadius="md" color="red" variant="outlined">
+          md border
+        </Button>
+        <Button borderRadius="lg" color="red" variant="subtle">
+          lg border
+        </Button>
+        <Button borderRadius="xl" color="red">
+          xl border
+        </Button>
+      </Flex>
+      <Button fullWidth>Full width</Button>
+    </div>
+  );
 };
 
-export const Primary = Template.bind({});
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-Primary.args = {
+export const Default = Template.bind({});
+
+Default.args = {
   children: "Button",
-  color: "red",
 };
