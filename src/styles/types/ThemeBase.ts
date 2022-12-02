f209fc13b5ad66174cd03794eab9c257f052e7ed
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-12-01 23:39:20 -0500

    Added colors

---
+++ src/styles/types/ThemeBase.ts
@@ -0,0 +1,7 @@
+import { SpinnersType } from "../../components/Feedback/Spinner/Spinner.types";
+
+export type ColorScheme = "dark" | "light";
+
+export interface ThemeBase {
+  theme: ColorScheme;
+}
