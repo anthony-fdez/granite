4fb865f6f1f5b9188926f878fa828e9c3a42cc12
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-12-28 18:05:08 -0500

    added wrapper to the tests

--- src/components/Buttons/Button/Button.test.tsx
+++ src/components/Buttons/Button/Button.test.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
 import Button from "./Button";
 import { IButtonProps } from "./Button.types";
-import ThemeProvider from "../../Theming/ThemeProvider";
 import itThrowsContextError from "../../../utils/tests/helpers/itThrowsContextError";
-
-const W = ThemeProvider;
+import { render, screen } from "../../../utils/tests/test-utils";
 
 const defaultProps: IButtonProps = {
   children: "test-button-title",
@@ -15,72 +12,48 @@ describe("Button", () => {
   itThrowsContextError(Button, defaultProps);
 
   it("renders a string as children", () => {
-    render(
-      <W>
-        <Button {...defaultProps} />
-      </W>,
-    );
+    render(<Button {...defaultProps} />);
 
     expect(screen.getByText(defaultProps.children)).toBeInTheDocument();
   });
 
   it("renders icon left", () => {
-    const button = render(
-      <W>
-        <Button {...defaultProps} iconLeft={<>L</>} />
-      </W>,
-    );
+    const button = render(<Button {...defaultProps} iconLeft={<>L</>} />);
 
     expect(button.getByTestId("button-icon-left")).toBeInTheDocument();
   });
 
   it("passes props to icon left", () => {
     const button = render(
-      <W>
-        <Button {...{ ...defaultProps, iconLeft: <>L</>, iconLeftProps: { "aria-label": "icon-left-aria-label" } }} />
-      </W>,
+      <Button {...defaultProps} iconLeft={<>L</>} iconLeftProps={{ "aria-label": "icon-left-aria-label" }} />,
     );
 
     expect(button.getByLabelText("icon-left-aria-label")).toBeInTheDocument();
   });
 
-  // it("renders icon right", () => {
-  //   const button = render(
-  //     <W>
-  //       <Button {...defaultProps} iconRight={<>R</>} />
-  //     </W>,
-  //   );
+  it("renders icon right", () => {
+    const button = render(<Button {...defaultProps} iconRight={<>R</>} />);
 
-  //   expect(button.getByTestId("button-icon-right")).toBeInTheDocument();
-  // });
+    expect(button.getByTestId("button-icon-right")).toBeInTheDocument();
+  });
 
-  // it("passes props to icon right", () => {
-  //   const button = render(
-  //     <W>
-  //       <Button {...defaultProps} iconRight={<>L</>} iconRightProps={{ "aria-label": "icon-right-aria-label" }} />
-  //     </W>,
-  //   );
+  it("passes props to icon right", () => {
+    const button = render(
+      <Button {...defaultProps} iconRight={<>L</>} iconRightProps={{ "aria-label": "icon-right-aria-label" }} />,
+    );
 
-  //   expect(button.getByLabelText("icon-right-aria-label")).toBeInTheDocument();
-  // });
+    expect(button.getByLabelText("icon-right-aria-label")).toBeInTheDocument();
+  });
 
-  // it("renders loading spinner", () => {
-  //   const button = render(
-  //     <W>
-  //       <Button {...defaultProps} loading />
-  //     </W>,
-  //   );
+  it("renders loading spinner", () => {
+    const button = render(<Button {...defaultProps} loading />);
 
-  //   expect(button.getByTestId("button-spinner")).toBeInTheDocument();
-  // });
+    expect(button.getByTestId("button-spinner")).toBeInTheDocument();
+  });
 
-  // it("is disabled", () => {
-  //   const button = render(
-  //     <W>
-  //       <Button {...defaultProps} disabled />
-  //     </W>,
-  //   );
+  it("is disabled", () => {
+    const button = render(<Button {...defaultProps} disabled />);
 
-  //   expect(button.getByLabelText(defaultProps.children)).toHaveAttribute("disabled");
-  // });
+    expect(button.getByLabelText(defaultProps.children)).toHaveAttribute("disabled");
+  });
 });
--- src/components/Buttons/CloseButton/CloseButton.test.tsx
+++ src/components/Buttons/CloseButton/CloseButton.test.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
 
-import ThemeProvider from "../../Theming/ThemeProvider";
 import CloseButton from "./CloseButton";
 import itThrowsContextError from "../../../utils/tests/helpers/itThrowsContextError";
-
-const W = ThemeProvider;
+import { render, screen } from "../../../utils/tests/test-utils";
 
 describe("CloseButton", () => {
   itThrowsContextError(CloseButton);
 
   it("renders close icon", () => {
-    render(
-      <W>
-        <CloseButton />
-      </W>,
-    );
+    render(<CloseButton />);
 
     expect(screen.getByTestId("CloseButton/close-icon")).toBeInTheDocument();
   });
