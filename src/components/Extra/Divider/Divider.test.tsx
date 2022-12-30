5ba81c288ff7751d8c62980e6804265c8053ac3d
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-12-30 14:05:46 -0500

    Fixed linting error

--- src/components/Extra/Divider/Divider.test.tsx
+++ src/components/Extra/Divider/Divider.test.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-
 import itThrowsContextError from "../../../utils/tests/helpers/itThrowsContextError";
-import { render, screen } from "../../../utils/tests/test-utils";
 import Divider from "./Divider";
 import { IDividerProps } from "./Divider.types";
 
