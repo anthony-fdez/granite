fa1e0055e83175b4e138be7bac803066217c2ec4
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-01-12 23:18:21 -0500

    Added tooltip positions

--- src/components/Tooltip/Tooltip.styles.ts
+++ src/components/Tooltip/Tooltip.styles.ts
@@ -29,7 +29,7 @@ export const getTooltipStyles = ({ getColor, position }: Props): SerializedStyle
         position: "absolute",
         left: "50%",
         transform: "translateX(-50%)",
-        padding: 10,
+        padding: 7,
         color: getColor({}).font,
         background: getColor({}).backgroundAccent,
         fontSize: 14,
@@ -46,7 +46,39 @@ export const getTooltipStyles = ({ getColor, position }: Props): SerializedStyle
       },
     },
     position === "top" && { ".Granite-Tooltip-tooltip": { bottom: "100%" } },
+    position === "top-start" && {
+      ".Granite-Tooltip-tooltip": {
+        left: "auto",
+        right: "100%",
+        bottom: "100%",
+        transform: "translateX(calc(100% + 5px))",
+      },
+    },
+    position === "top-end" && {
+      ".Granite-Tooltip-tooltip": {
+        right: "auto",
+        left: "100%",
+        bottom: "100%",
+        transform: "translateX(calc(-100% - 5px))",
+      },
+    },
     position === "bottom" && { ".Granite-Tooltip-tooltip": { top: "100%" } },
+    position === "bottom-start" && {
+      ".Granite-Tooltip-tooltip": {
+        left: "auto",
+        right: "100%",
+        top: "100%",
+        transform: "translateX(calc(100% + 5px))",
+      },
+    },
+    position === "bottom-end" && {
+      ".Granite-Tooltip-tooltip": {
+        right: "auto",
+        left: "100%",
+        top: "100%",
+        transform: "translateX(calc(-100% - 5px))",
+      },
+    },
     position === "left" && {
       ".Granite-Tooltip-tooltip": {
         left: "auto",
@@ -55,11 +87,43 @@ export const getTooltipStyles = ({ getColor, position }: Props): SerializedStyle
         transform: "translateX(0px) translateY(-50%)",
       },
     },
+    position === "left-end" && {
+      ".Granite-Tooltip-tooltip": {
+        left: "auto",
+        right: "100%",
+        top: "100%",
+        transform: "translateX(0px) translateY(calc(-100% - 5px))",
+      },
+    },
+    position === "left-start" && {
+      ".Granite-Tooltip-tooltip": {
+        left: "auto",
+        right: "100%",
+        bottom: "100%",
+        transform: "translateX(0px) translateY(calc(100% + 5px))",
+      },
+    },
     position === "right" && {
       ".Granite-Tooltip-tooltip": {
         left: "100%",
         top: "50%",
         transform: "translateX(0px) translateY(-50%)",
+      },
+    },
+    position === "right-end" && {
+      ".Granite-Tooltip-tooltip": {
+        right: "auto",
+        left: "100%",
+        top: "100%",
+        transform: "translateX(0px) translateY(calc(-100% - 5px))",
+      },
+    },
+    position === "right-start" && {
+      ".Granite-Tooltip-tooltip": {
+        right: "auto",
+        left: "100%",
+        bottom: "100%",
+        transform: "translateX(0px) translateY(calc(100% + 5px))",
       },
     },
   );
